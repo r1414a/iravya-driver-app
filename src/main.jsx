097847f6bb86105f486c6aad8fe47c8d73e0227a
Rupesh Chincholkar <70930255+r1414a@users.noreply.github.com>
2026-04-13 10:47:59 +0530

    Implement lazy loading for DriverDashboard component
    Refactor DriverDashboard import to use lazy loading with Suspense for improved code splitting and loading state.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense  } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import DriverDashboard from './App.jsx'
+const DriverDashboard = lazy(() => import('./App.jsx'))
 // import DriverDashboard from './dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Suspense fallback={<div>Loading....</div>}>
     <DriverDashboard />
+      </Suspense>
   </StrictMode>,
 )
 
